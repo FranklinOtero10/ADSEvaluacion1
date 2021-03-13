96cab5ddbbbec2468f47f3aae93156bf75148aa3
FranklinOtero10 <franklin.otero6@gmail.com>
2021-03-13 16:40:26 -0600

    Evaluacion 1 completa

---
+++ ADSEvaluacion1/Controllers/OperacionesController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ADSEvaluacion1.Models;
+
+namespace ADSEvaluacion1.Controllers
+{
+    public class OperacionesController : Controller
+    {
+        // GET: Operaciones/Operaciones_Aritmeticas
+        public ActionResult Operaciones_Aritmeticas(int operar = 0, Double valor1 = 0, Double valor2= 0)
+        {
+            Operaciones oper = new Operaciones();
+
+            switch (operar)
+            {
+                case 1:
+                    oper.operaracion = "Suma";
+                    oper.resultado = valor1+valor2;
+                    break;
+                case 2:
+                    oper.operaracion = "Resta";
+                    oper.resultado = valor1 - valor2;
+                    break;
+                case 3:
+                    oper.operaracion = "Multiplicación";
+                    oper.resultado = valor1 * valor2;
+                    break;
+                case 4:
+                    oper.operaracion = "División";
+                    oper.resultado = valor1 / valor2;
+                    break;
+                default:
+                    oper.resultado = 0;
+                    break;
+            }
+
+            return View(oper);
+        }
+
+        
+    }
+}
---
+++ ADSEvaluacion1/Models/Operaciones.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ADSEvaluacion1.Models
+{
+    public class Operaciones
+    {
+       public String operaracion { get; set; }
+       public double resultado { get; set; }
+
+    }
+}
